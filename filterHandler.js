30e4b918b18ed3ef2d0580a8b5a9168f662e20b2
Zaqueu Alves <69696525+zaqueu-git@users.noreply.github.com>
2022-06-09 19:43:05 -0300

    Update and rename eventHandlersFilters.js to filterHandler.js

---
+++ filterHandler.js
@@ -0,0 +1,47 @@
+function filterHandler() {
+    this.path = window.location;
+    this.url = new URL(this.path);
+
+    this.setInputs = function() {
+        this.url.searchParams.forEach(list);
+
+        function list(value, key) {
+            var input = document.querySelector('.js-' + key + '-field');
+
+            if (input) {
+                input.value = value;
+            }
+        }
+    };
+
+    this.getParams = function(fieldName) {
+        let param = this.url.searchParams.get(fieldName);
+
+        if (param) {
+            return param;
+        }
+        return "";
+    }
+
+    this.addParams = function(listParams) {
+        for (var prop in listParams) {
+            this.setParams(prop, listParams[prop]);
+        }
+    }
+
+    this.setParams = function(fieldName, fieldValue) {
+        console.log(fieldValue);
+
+        if (fieldValue != "") {
+            this.url.searchParams.set(fieldName, fieldValue);
+        } else {
+            this.url.searchParams.delete(fieldName);
+        }
+
+        window.history.pushState({}, '', this.url);
+    }
+
+    this.reload = function() {
+        location.reload();
+    }
+}
